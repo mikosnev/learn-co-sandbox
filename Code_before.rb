de558bb369c9a2d731fa82bd65c5291c4f70df76
Sokim Ven <sokimv@gmail.com>
2020-03-15 15:16:02 +0000

    Automatically backed up by Learn

---
+++ Code_before.rb
@@ -0,0 +1,13 @@
+run_code_inside = TRUE
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after...end"
+
+run_code_inside = FALSE
+puts "Code before if..."
+if run_code_inside
+  puts code inside"
+end 
+puts Code after...end"
